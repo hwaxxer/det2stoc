53692c373dc1321baacffa067211697f1364b614
Martin Hwasser <martin.hwasser@gmail.com>
2019-09-11 00:59:23 +0200

    Use consistent data structure.

--- test_yumi_ppo2.py
+++ test_yumi_ppo2.py
@@ -27,15 +27,14 @@ assert not args.render or (args.render and 1 == n_cpu), 'Cannot render when usin
 real = False
 uniform = False
 
-DYNAMICS_LO = [-0.085, 0.1]
-DYNAMICS_HI = [0.085, 1]
+DYNAMICS_LO = [0.1, -0.085]
+DYNAMICS_HI = [1, 0.085]
 
 if real:
-    params = [0.35, -0.08]
-    params = [0.4, 0.08]
+    params = [0.4, -0.08]
 else:
-    params = [(0.1, 0.5), (-0.085, 0.085)]
-    params = [(0.3424, 0.0772), (-0.0521, 0.0153)]
+    params = [(0.1, 0.5), (-0.09, 0.09)]
+    params = [(0.3248, 0.1152), (0.0788, 0.0284)]
 
 def truncnorm(lo, hi, loc, scale):
     return stats.truncnorm((lo-loc)/scale, (hi-loc)/scale, loc=loc, scale=scale).rvs()
@@ -61,58 +60,42 @@ def make_env(render, i, seed=0):
 
 yumis = [make_env(args.render, i, seed=i) for i in range(n_cpu)]
 env = SubprocVecEnv(yumis)
-env = DummyVecEnv(yumis)
 
 model = PPO2.load(args.model_path, env=env, policy=MlpPolicy)
 
-n_episodes = 300 if real else 1000
+n_episodes = 100 if real else 5000
 
 horizon = env.env_method('get_horizon')[0]
+print('horizon: ', horizon)
+
+states, actions, next_states, parameters, steps = [], [], [], [], []
 
 
-states, actions, next_states, dynamics = [], [], [], []
+n_steps = (horizon*n_episodes)//n_cpu
 
-'''
+print('n steps: ', n_steps)
 obs = env.reset()
-for ep in range(n_episodes):
+for ep in range(n_steps):
+    states.extend(obs)
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, done, info = env.step(action)
+    actions.extend(action)
+    next_states.extend(obs)
     dynamics = env.env_method('get_dynamics')
-    print('Dynamics: ', dynamics)
-    for step in range(horizon):
-        states.append(obs)
-        action, _states = model.predict(obs, deterministic=True)
-        obs, rewards, done, info = env.step(action)
-        actions.append(action)
-        next_states.append(obs)
-        dynamics.append(dynamics)
+    parameters.extend(dynamics)
+    steps.append(env.env_method('get_step'))
 
-data = {'states': np.array(states),
+data = {
+        'states': np.array(states),
         'actions': np.array(actions),
         'next_states': np.array(next_states),
-        'dynamics': np.array(dynamics)}
-with open(filename, 'wb') as f:
-    np.save(f, data)
+        'parameters': np.array(parameters),
+        'steps': np.array(steps)
+        }
 
-'''
 mode_str = 'real' if real else 'fake'
 dist_str = 'uniform' if uniform else 'normal'
-filename = 'data/yumi/low_friction_center_box/{}_yumi_ppo_{}_fri_{}_com{}_deterministic_{}.npz'.format(mode_str, n_episodes, str(params[0]), str(params[1]), dist_str)
-
-observations = []
-obs = env.reset()
-for ep in range(n_episodes//n_cpu):
-    dynamics = env.env_method('get_dynamics')
-    print('Dynamics: ', dynamics)
-    for step in range(horizon):
-        l = [[] for _ in range(n_cpu)]
-        for i in range(n_cpu):
-            l[i].append(obs[i])
-        action, _states = model.predict(obs, deterministic=True)
-        obs, rewards, done, info = env.step(action)
-        for i in range(n_cpu):
-            l[i].extend([action[i], obs[i]])
-            l[i].append(dynamics[i])
-        observations.extend([np.array(x) for x in l])
+filename = 'data/yumi/{}_yumi_ppo_{}_fri_{}_com{}_{}.npz'.format(mode_str, n_episodes, str(params[0]), str(params[1]), dist_str)
 
 with open(filename, 'wb') as f:
-    np.save(f, observations)
-
+    np.save(f, data)
