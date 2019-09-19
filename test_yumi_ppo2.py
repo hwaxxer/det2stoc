20f081bf46c9c5ba034f5ea38b0f840f67ffd429
Martin Hwasser <martin.hwasser@gmail.com>
2019-09-19 12:54:37 +0200

    Run stochastic.

--- test_yumi_ppo2.py
+++ test_yumi_ppo2.py
@@ -25,16 +25,19 @@ n_cpu = args.n_cpu
 assert not args.render or (args.render and 1 == n_cpu), 'Cannot render when using more than one cpu.'
 
 real = False
-uniform = False
+uniform = True
 
 DYNAMICS_LO = [0.1, -0.085]
 DYNAMICS_HI = [1, 0.085]
 
 if real:
-    params = [0.4, -0.08]
+    params = [0.35, 0.08]
 else:
-    params = [(0.1, 0.5), (-0.09, 0.09)]
-    params = [(0.3248, 0.1152), (0.0788, 0.0284)]
+    params = [(0.1, 0.9), (-0.09, 0.09)]
+    #params = [(0.3248, 0.1152), (0.0788, 0.0284)]
+    #params = [(0.69048023, 0.3243859), (0.12308636, 0.04219555)]
+    #params = [(0.82035035,0.108042516), (0.09504627, 0.021840865)]
+    #params = [(0.14548126, 0.46216267), (0.03220001, 0.057417072)]
 
 def truncnorm(lo, hi, loc, scale):
     return stats.truncnorm((lo-loc)/scale, (hi-loc)/scale, loc=loc, scale=scale).rvs()
@@ -53,11 +56,14 @@ def dynamics_generator(seed):
 
 def make_env(render, i, seed=0):
     def create_yumi():
-        logging_level = logging.DEBUG if args.debug else logging.INFO
-        return YuMi(path=path, task=args.task, render=render, seed=seed, dynamics=dynamics_generator(seed), logging_level=logging_level)
+        return YuMi(path=path, task=args.task,
+                render=render, seed=seed,
+                dynamics=dynamics_generator(seed),
+                logging_level=logging_level)
 
     return create_yumi
 
+logging_level = logging.DEBUG if args.debug else logging.INFO
 yumis = [make_env(args.render, i, seed=i) for i in range(n_cpu)]
 env = SubprocVecEnv(yumis)
 
@@ -65,19 +71,17 @@ model = PPO2.load(args.model_path, env=env, policy=MlpPolicy)
 
 n_episodes = 100 if real else 5000
 
-horizon = env.env_method('get_horizon')[0]
-print('horizon: ', horizon)
-
 states, actions, next_states, parameters, steps = [], [], [], [], []
 
-
+horizon = env.env_method('get_horizon')[0]
 n_steps = (horizon*n_episodes)//n_cpu
 
-print('n steps: ', n_steps)
+deterministic = False
+
 obs = env.reset()
 for ep in range(n_steps):
     states.extend(obs)
-    action, _states = model.predict(obs, deterministic=True)
+    action, _states = model.predict(obs, deterministic=deterministic)
     obs, rewards, done, info = env.step(action)
     actions.extend(action)
     next_states.extend(obs)
@@ -95,7 +99,12 @@ data = {
 
 mode_str = 'real' if real else 'fake'
 dist_str = 'uniform' if uniform else 'normal'
-filename = 'data/yumi/{}_yumi_ppo_{}_fri_{}_com{}_{}.npz'.format(mode_str, n_episodes, str(params[0]), str(params[1]), dist_str)
+basename = os.path.basename(args.model_path)
+filename = 'new_data/{}/yumi/{}_yumi_ppo_{}_fri_{}_com{}_{}_det{}.npz'.format(basename,
+        mode_str, n_episodes, str(params[0]), str(params[1]), dist_str,
+        int(deterministic))
+
+os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 with open(filename, 'wb') as f:
     np.save(f, data)
