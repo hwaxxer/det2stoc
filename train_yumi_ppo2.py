26139df84393c4995463dc444091e11b0323110d
Martin Hwasser <martin.hwasser@gmail.com>
2019-09-24 14:23:12 +0200

    Update training script to use logging and accept learning rate.

--- train_yumi_ppo2.py
+++ train_yumi_ppo2.py
@@ -7,6 +7,7 @@ from stable_baselines import PPO2
 from yumi import *
 import argparse
 from scipy import stats
+import logging
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -14,6 +15,7 @@ parser.add_argument('--render', help='render', default=False, action='store_true
 parser.add_argument('--checkpoint-path', help='path to previous checkpoint', type=str, default=None)
 parser.add_argument('--xml-path', help='path to model xml', default='models/cheezit.xml')
 parser.add_argument('--task', help='task to solve', default=0, type=int)
+parser.add_argument('--lr', help='learning_rate', default=0.00025, type=float)
 parser.add_argument('--debug', help='to print debugging or not', default=False, action='store_true')
 
 args = parser.parse_args()
@@ -21,16 +23,23 @@ args = parser.parse_args()
 log_dir = "/tmp/yumi/ppo2/{}".format(int(time.time()))
 os.makedirs(log_dir, exist_ok=True)
 
+# Log arguments
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(message)s",
+    handlers=[logging.FileHandler(os.path.join(log_dir, 'log.txt')),
+              logging.StreamHandler(sys.stdout)])
+logging.info(args)
+
 path = args.xml_path
 name = os.path.basename(path)
 
-n_cpu = 6
+n_cpu = 12
 
-params  = [0.4, 0.08]
+params  = [0.5, -0.07]
 
 def make_env(path, render, i, seed=0):
     def create_yumi():
-        dynamics = lambda: params
+        dynamics = lambda: params + np.random.uniform(0.01*np.array(params)) # 1 % noise
         logging_level = logging.DEBUG if args.debug else logging.INFO
         return YuMi(path, task=args.task, render=render, seed=seed, dynamics=dynamics, logging_level=logging_level)
 
@@ -47,22 +56,28 @@ def callback(_locals, _globals):
     global n_steps
 
     n_steps += 1
-    if n_steps % 10 == 0 or n_steps < 10:
-        print('callback!')
+    if n_steps % 50 == 0 or n_steps == 10:
         if finetune:
-            save_path = os.path.join(log_dir,
-                    'ppo2_finetune_{}_task_{}_{}.npy'.format(os.path.basename(args.checkpoint_path), args.task, n_steps))
+            save_name = 'ppo2_finetune_{}_task_{}_fri{}_com{}_{}.npy'.format(
+                    os.path.basename(args.checkpoint_path),
+                    args.task, params[0], params[1], n_steps)
+            save_path = os.path.join(log_dir, save_name)
         else:
-            save_path = os.path.join(log_dir, 'ppo2_{}_task_{}_{}_alpha0.5.npy'.format(name, args.task, n_steps))
+            save_path = os.path.join(log_dir, 'ppo2_{}_task_{}_fri{}_com{}_{}.npy'.format(name,
+                args.task, params[0], params[1], n_steps))
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         model.save(save_path)
 
     return True
 
 if args.checkpoint_path is None:
-    model = PPO2(MlpPolicy, env, n_steps=1024, verbose=1, tensorboard_log=log_dir)
+    policy_kwargs = dict(net_arch=[128]*3)
+    model = PPO2(MlpPolicy, env, learning_rate=args.lr,
+                 verbose=1, gamma=0.995, lam=0.95,
+                 policy_kwargs=policy_kwargs, tensorboard_log=log_dir)
 else:
-    model = PPO2.load(args.checkpoint_path, env=env, steps=1024, policy=MlpPolicy, tensorboard_log=log_dir)
+    model = PPO2.load(args.checkpoint_path, env=env, learning_rate=args.lr,
+                      policy=MlpPolicy, tensorboard_log=log_dir)
 
 model.learn(total_timesteps=total_timesteps, callback=callback)
 
